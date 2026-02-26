aa4f66b79525cfea3768732ff61ed6eb0147da9b
sean <hanvv3@gmail.com>
2026-02-26 16:02:29 +0900

    edited fastapi endpoint: infer

--- wt_day10-fastapi.py
+++ wt_day10-fastapi.py
@@ -10,7 +10,6 @@ from PIL import Image
 
 
 
-model = None
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 transform_test = T.Compose(
     [
@@ -25,9 +24,7 @@ transform_test = T.Compose(
 async def lifespan(app: FastAPI):
     # 🔥 startup 영역
     print("============= Model loading initiated =============")
-    
-    global model
-    
+        
     model = models.resnet34(weights=models.ResNet34_Weights.DEFAULT)
     model.fc = nn.Linear(model.fc.in_features, 3)
 
@@ -65,6 +62,7 @@ async def infer(file:UploadFile = File(...)):                   # body: form-dat
     
     allowed_exts = ["jpg", "jpeg", "png", "bmp", "webp"]
     
+    
     if not file.filename:
         return {"error": "파일명이 없습니다."}
     
@@ -73,24 +71,27 @@ async def infer(file:UploadFile = File(...)):                   # body: form-dat
     if ext not in allowed_exts:
         return {"error" : "이미지 파일을 업로드하세요!"}
     
+    
     # 무조건 데이터는 저장해야 한다. MLops관점에서: 평가 및 재학습
     # 이미지를 uuid로 고유한 이름으로 변경한 뒤 서버가 아닌 클라우드에 저장해야함
     newfile_name = f"{uuid.uuid4()}.{ext}"
     file_path = os.path.join("upload_img", newfile_name)
     
+    img = await file.read()
+    
     with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)    # file.file은 파일obj, buffer는 새로운 파일obj
+        buffer.write(img)           # file.file은 파일obj, buffer는 새로운 파일obj
         
     #------------------- 추론 코드 ------------------------
     
     #img = await file.read()    # 위에서 이미 파일 버퍼를 읽어왔기 때문에 비어있어서 에러남.
-    #pil_img = Image.open(io.BytesIO(img)).convert("RGB")
-    pil_img = Image.open(file_path).convert("RGB")
+    pil_img = Image.open(io.BytesIO(img)).convert("RGB")
+    #pil_img = Image.open(file_path).convert("RGB")
     
     input_tensor = transform_test(pil_img).unsqueeze(0).to(device)
     
     with torch.no_grad():
-        pred = model(input_tensor)
+        pred = app.state.model(input_tensor)
         result = torch.argmax(pred, dim=1).item()
     
     model_class = ['해린','장원영','카리나']
@@ -100,3 +101,5 @@ async def infer(file:UploadFile = File(...)):                   # body: form-dat
         "index": result,
         "filename": newfile_name
     }
+    
+    
